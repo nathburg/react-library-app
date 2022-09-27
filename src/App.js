ffc96e151b5617b4c7838b769e5a39cd74d34404
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-26 18:13:57 -0700

    book details load

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import BookList from './components/book/BookList';
+import Home from './components/home/Home';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import BookDetail from './components/book/BookDetail';
+import NotFound from './components/notfound/NotFound';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route path='/books/:id' component={BookDetail} /> 
+        <Route path='/books' component={BookList} />
+        <Route path='/' component={Home} />
+        <Route path='*' component={NotFound} />
+      </Switch>
+      
     </main>
   );
 }
